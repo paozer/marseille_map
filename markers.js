e26f968f1cfde886219815a6af42b2c68c776246
paozer <33629278+paozer@users.noreply.github.com>
2020-11-24 17:39:51 +0100

    initial commit

---
+++ markers.js
@@ -0,0 +1,24 @@
+markers = [
+    {
+        "name": "Le vieux port",
+        "text": "description du vieux port",
+        "audio": "audio/example.mp3",
+        "img": "images/example.jpg",
+        "alt": "image is not available",
+        "width": "",
+        "heigth": "",
+        "lat": 5,
+        "lng": 9.5
+    },
+    {
+        "name": "Random spot",
+        "text": "This is the random spots abstract. <br />With a breakline in the middle.",
+        "audio": "audio/example.mp3",
+        "img": "this_image_does_not_exist.jpg",
+        "alt": "image is not available",
+        "width": "100px",
+        "heigth": "100px",
+        "lat": 10,
+        "lng": 4
+    }
+];
